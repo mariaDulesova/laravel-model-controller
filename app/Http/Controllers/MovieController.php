44c7681f54ed22c1d05698c2b2a27b8892c92b85
mariaDulesova <maria.dulesova@gmail.com>
2021-07-14 14:29:04 +0200

    Data from MySQL

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        $title = 'Movies from our Database';
+        $movies = Movie::all();
+        return view('home', compact('title','movies'));
+    }
 }
