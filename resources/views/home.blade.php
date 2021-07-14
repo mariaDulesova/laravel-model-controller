19734202d12d88ba985cbac7ba5493fc8025e7ec
mariaDulesova <maria.dulesova@gmail.com>
2021-07-14 15:43:31 +0200

    style added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,74 +5,27 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Movie DataBase</title>
-
+{{-- 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet"> --}}
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <h1> {{ $title }} </h1>
-        @foreach ($movies as $movie)
-            <div class="card">
-                <h2>{{ $movie->title}}</h2>
-                <h4>{{ $movie->mationality}}</h4>
-                <h4>{{ $movie->date}}</h4>
-                <h4>{{ $movie->vote}}</h4>
+        <section class="wrap">
+            <h1> {{ $title }} </h1>
+            <div class="card-container">
+                @foreach ($movies as $movie)
+                    <div class="card">
+                        <h4>{{ $movie->title}}</h4>
+                        <h6>{{ $movie->nationality}}</h6>
+                        <h6>{{ $movie->date}}</h6>
+                        <h6>{{ $movie->vote}}</h6>
+                    </div> 
+                @endforeach
             </div> 
-        @endforeach
-        
+        </section>
     </body>
 </html>
